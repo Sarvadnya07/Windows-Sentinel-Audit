6d4062e1268805c07bdf83ad346872f081e6e96b
Sarvadnya <sarvadnyasonkambale0@gmail.com>
2026-09-19 06:02:30 +0530

    refactor: harden Modules/ReportEngine.psm1

--- Modules/ReportEngine.psm1
+++ Modules/ReportEngine.psm1
@@ -1,6 +1,21 @@
 Set-StrictMode -Version Latest
 
+<#
+.SYNOPSIS
+Constructs the combined AbuserHunter report object.
+
+.DESCRIPTION
+Creates an in-memory report representation without modifying the target
+system.
+#>
 function New-AbuserHunterReport {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+        'PSUseShouldProcessForStateChangingFunctions',
+        '',
+        Scope = 'Function',
+        Target = '*',
+        Justification = 'This function constructs an in-memory report only.'
+    )]
     param(
         [Parameter(Mandatory)]$ProcessAudit,
         [Parameter(Mandatory)]$NetworkAudit,
@@ -13,23 +28,25 @@ function New-AbuserHunterReport {
 
     $Summary = [PSCustomObject]@{
         ComputerName = $env:COMPUTERNAME
-        UserName     = $env:USERNAME
-        GeneratedAt  = Get-Date
+        UserName = $env:USERNAME
+        GeneratedAt = Get-Date
         ProcessCount = $ProcessAudit.Data.Count
         NetworkCount = $NetworkAudit.Data.Count
         ServiceCount = $ServiceAudit.Data.Count
         PersistenceCount = $PersistenceAudit.Data.Count
-        HighRiskCount = ($RiskResults | Where-Object RiskScore -ge 60).Count
+        HighRiskCount = (
+            $RiskResults | Where-Object RiskScore -ge 60
+        ).Count
     }
 
     return [PSCustomObject]@{
-        Summary      = $Summary
-        Processes    = $ProcessAudit.Data
-        Network      = $NetworkAudit.Data
-        Services     = $ServiceAudit.Data
-        Signatures   = $SignatureAudit.Data
-        Persistence  = $PersistenceAudit.Data
-        Risk         = $RiskResults
+        Summary = $Summary
+        Processes = $ProcessAudit.Data
+        Network = $NetworkAudit.Data
+        Services = $ServiceAudit.Data
+        Signatures = $SignatureAudit.Data
+        Persistence = $PersistenceAudit.Data
+        Risk = $RiskResults
         Investigation = $Investigation
     }
 }
